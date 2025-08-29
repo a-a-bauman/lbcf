34ac572d640e35bbfa7bd46d318ed05ea86a3d50
Aaron Bauman <aaron.a.bauman@gmail.com>
2025-08-29 14:13:47 -0400

    Also create HTML output and improves uploading

--- scripts/build-aaron-lbc-pdf.sh
+++ scripts/build-aaron-lbc-pdf.sh
@@ -3,11 +3,14 @@ set -e
 
 ROOT_DIR=.
 BUILD_DIR="$ROOT_DIR/build"
+PDF_DIR="$BUILD_DIR/pdf"
+HTML_DIR="$BUILD_DIR/html"
 DOC_DIR="$ROOT_DIR/aaron"
 # Define the output PDF file name
 OUTPUT_PDF="$BUILD_DIR/lbc1689_aab.pdf"
 
-mkdir -p "$BUILD_DIR"
+mkdir -p "$PDF_DIR"
+mkdir -p "$HTML_DIR"
 
 # Create a temporary markdown file to concatenate all chapters
 TEMP_MD="$BUILD_DIR/lbc1689_aab.md"
@@ -15,13 +18,15 @@ TEMP_MD="$BUILD_DIR/lbc1689_aab.md"
 # Start with the index
 echo "# Index" > $TEMP_MD
 cat $DOC_DIR/lbc1689-index.md >> $TEMP_MD
-pandoc $DOC_DIR/lbc1689-index.md -o $BUILD_DIR/lbc1689-index.pdf
+pandoc $DOC_DIR/lbc1689-index.md -o $PDF_DIR/lbc1689-index.pdf
+pandoc $DOC_DIR/lbc1689-index.md -o $HTML_DIR/lbc1689-index.html
 printf "\n\n" >> $TEMP_MD
 
 # Add the introduction
 echo "# Introduction" >> $TEMP_MD
 cat $DOC_DIR/lbc1689-introduction.md >> $TEMP_MD
-pandoc $DOC_DIR/lbc1689-introduction.md -o $BUILD_DIR/lbc1689-introduction.pdf
+pandoc $DOC_DIR/lbc1689-introduction.md -o $PDF_DIR/lbc1689-introduction.pdf
+pandoc $DOC_DIR/lbc1689-introduction.md -o $HTML_DIR/lbc1689-introduction.html
 echo "\n\n" >> $TEMP_MD
 
 # Concatenate all chapter files
@@ -32,7 +37,8 @@ for chapter in $DOC_DIR/lbc1689-ch*.md; do
     echo "\newpage" >> $TEMP_MD
     cat "$chapter" >> $TEMP_MD
     echo "\n\n" >> $TEMP_MD
-    pandoc $chapter -o "$BUILD_DIR/libc1689-ch$padded_index.pdf"
+    pandoc $chapter -o "$PDF_DIR/libc1689-ch$padded_index.pdf"
+    pandoc $chapter -o "$HTML_DIR/libc1689-ch$padded_index.html"
 done
 
 # Add the signatories at the end
@@ -44,13 +50,13 @@ echo "\n\n" >> $TEMP_MD
 echo "# Addendums" >> $TEMP_MD
 cat $DOC_DIR/Addendums.md >> $TEMP_MD
 
-ls -al $DOC_DIR
-ls -al $BUILD_DIR
+echo "list $DOC_DIR"
+ls -alR $DOC_DIR
+echo "list $BUILD_DIR"
+ls -alR $BUILD_DIR
 
 # Convert the combined markdown file to PDF using pandoc
 #pandoc $TEMP_MD -o $OUTPUT_PDF
-
-zip $BUILD_DIR/lbc1689_aab.zip $BUILD_DIR/*.pdf
 
 echo "PDF generated: $OUTPUT_PDF"
 ls -al $BUILD_DIR
