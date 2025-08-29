04512ba8377ba6b4b016785586353bf7d2e0dfb8
Aaron Bauman <aaron.a.bauman@gmail.com>
2025-08-29 08:18:50 -0400

    Uses a document dir var

--- scripts/build-aaron-lbc-pdf.sh
+++ scripts/build-aaron-lbc-pdf.sh
@@ -2,6 +2,7 @@
 
 ROOT_DIR=../..
 BUILD_DIR=build
+DOC_DIR=aaron
 # Define the output PDF file name
 OUTPUT_PDF="$ROOT_DIR/$BUILD_DIR/lbc1689_aab.pdf"
 
@@ -12,16 +13,16 @@ TEMP_MD="$ROOT_DIR/$BUILD_DIR/lbc1689_aab.md"
 
 # Start with the index
 echo "# Index" > $TEMP_MD
-cat ../../aaron/lbc1689-index.md >> $TEMP_MD
+cat $DOC_DIR/lbc1689-index.md >> $TEMP_MD
 printf "\n\n" >> $TEMP_MD
 
 # Add the introduction
 echo "# Introduction" >> $TEMP_MD
-cat ../../aaron/lbc1689-introduction.md >> $TEMP_MD
+cat $DOC_DIR/lbc1689-introduction.md >> $TEMP_MD
 echo "\n\n" >> $TEMP_MD
 
 # Concatenate all chapter files
-for chapter in ../../aaron/lbc1689-ch*.md; do
+for chapter in $DOC_DIR/lbc1689-ch*.md; do
     echo "\newpage" >> $TEMP_MD
     cat "$chapter" >> $TEMP_MD
     echo "\n\n" >> $TEMP_MD
@@ -29,12 +30,12 @@ done
 
 # Add the signatories at the end
 echo "# Signatories" >> $TEMP_MD
-cat ../../aaron/lbc1689-signatories.md >> $TEMP_MD
+cat $DOC_DIR/lbc1689-signatories.md >> $TEMP_MD
 echo "\n\n" >> $TEMP_MD
 
 # Add the addendums at the last pages
 echo "# Addendums" >> $TEMP_MD
-cat ../../aaron/Addendums.md >> $TEMP_MD
+cat $DOC_DIR/Addendums.md >> $TEMP_MD
 
 # Convert the combined markdown file to PDF using pandoc
 pandoc $TEMP_MD -o $OUTPUT_PDF
