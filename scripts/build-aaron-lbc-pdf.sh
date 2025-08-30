bf168e248b0d3db308f9e230d8c197d3b423423a
Aaron Bauman <aaron.a.bauman@gmail.com>
2025-08-30 15:21:34 -0400

    Fixes the reference to the Addendum; creates a combined PDF document; removes combined markdown file

--- scripts/build-aaron-lbc-pdf.sh
+++ scripts/build-aaron-lbc-pdf.sh
@@ -12,53 +12,35 @@ OUTPUT_PDF="$BUILD_DIR/lbc1689_aab.pdf"
 mkdir -p "$PDF_DIR"
 mkdir -p "$HTML_DIR"
 
-# Create a temporary markdown file to concatenate all chapters
-TEMP_MD="$BUILD_DIR/lbc1689_aab.md"
-
-cat $DOC_DIR/OpeningThoughts.md >> $TEMP_MD
 pandoc $DOC_DIR/OpeningThoughts.md --output $PDF_DIR/OpeningThoughts.pdf
 pandoc --standalone $DOC_DIR/OpeningThoughts.md --to html --output $HTML_DIR/OpeningThoughts
-printf "\n\n" >> $TEMP_MD
 
-cat $DOC_DIR/lbc1689-index.md >> $TEMP_MD
 pandoc $DOC_DIR/lbc1689-index.md --output $PDF_DIR/lbc1689-index.pdf
 pandoc --standalone $DOC_DIR/lbc1689-index.md --to html --output $HTML_DIR/lbc1689-index
-printf "\n\n" >> $TEMP_MD
 
 # Add the introduction
-echo "# Introduction" >> $TEMP_MD
-cat $DOC_DIR/lbc1689-introduction.md >> $TEMP_MD
 pandoc $DOC_DIR/lbc1689-introduction.md --output $PDF_DIR/lbc1689-introduction.pdf
 pandoc --standalone $DOC_DIR/lbc1689-introduction.md --to html --output $HTML_DIR/lbc1689-introduction
-echo "\n\n" >> $TEMP_MD
 
 # Concatenate all chapter files
 index=0
 for chapter in $DOC_DIR/lbc1689-ch*.md; do
     index=$((index + 1))
     padded_index=$(printf "%02d" $index)
-    echo "\newpage" >> $TEMP_MD
-    cat "$chapter" >> $TEMP_MD
-    echo "\n\n" >> $TEMP_MD
     pandoc $chapter --output "$PDF_DIR/lbc1689-ch$padded_index.pdf"
     pandoc --standalone $chapter --to html --output "$HTML_DIR/lbc1689-ch$padded_index"
 done
 
-echo "# Signatories" >> $TEMP_MD
-cat $DOC_DIR/lbc1689-signatories.md >> $TEMP_MD
 pandoc $DOC_DIR/lbc1689-signatories.md --output $PDF_DIR/lbc1689-signatories.pdf
 pandoc --standalone $DOC_DIR/lbc1689-signatories.md --to html --output $HTML_DIR/lbc1689-signatories
-echo "\n\n" >> $TEMP_MD
 
-echo "# Addendums" >> $TEMP_MD
-cat $DOC_DIR/Addendum.md >> $TEMP_MD
-pandoc $DOC_DIR/Addendum.md --output $PDF_DIR/lbc1689-Addendums.pdf
-pandoc --standalone $DOC_DIR/Addendum.md --to html --output $HTML_DIR/lbc1689-Addendums
+pandoc $DOC_DIR/Addendum.md --output $PDF_DIR/lbc1689-Addendum.pdf
+pandoc --standalone $DOC_DIR/Addendum.md --to html --output $HTML_DIR/lbc1689-Addendum
+
+# Build one combined PDF files
+pandoc $DOC_DIR/OpeningThoughts.md $DOC_DIR/lbc1689-index.md $DOC_DIR/lbc1689-introduction.md $(ls $DOC_DIR/lbc1689-ch*.md) $DOC_DIR/lbc1689-signatories.md $DOC_DIR/Addendum.md --output $PDF_DIR/lbc1689-aab.pdf
 
 echo "list $DOC_DIR"
 ls -alR $DOC_DIR
 echo "list $BUILD_DIR"
 ls -alR $BUILD_DIR
-
-# Convert the combined markdown file to PDF using pandoc
-#pandoc $TEMP_MD --output $OUTPUT_PDF
