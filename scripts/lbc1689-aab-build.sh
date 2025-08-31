04e1cb61be8169306009ad3b068f82f9f3790c99
Aaron Bauman <aaron.a.bauman@gmail.com>
2025-08-30 23:41:56 -0400

    Adds yellow highlighting to indicate annotations

--- scripts/lbc1689-aab-build.sh
+++ scripts/lbc1689-aab-build.sh
@@ -7,6 +7,7 @@ init() {
   STAGING_DIR="$BUILD_DIR/stagingHtml"
   HTML_DIR="$BUILD_DIR/html"
   DOC_DIR="$ROOT_DIR/aaron"
+  STYLES_DIR="$DOC_DIR/styles"
   GENERATED_PDF="$BUILD_DIR/lbc1689-aab.pdf"
   TEMP_MD="$BUILD_DIR/temp.md"
 
@@ -31,19 +32,19 @@ prepareMarkdownForHtml() {
 
 buildHtml() {
   prepareMarkdownForHtml
-  pandoc --standalone $STAGING_DIR/aab-TitlePage.md --to html --output $HTML_DIR/aab-TitlePage
-  pandoc --standalone $STAGING_DIR/aab-OpeningThoughts.md --to html --output $HTML_DIR/aab-OpeningThoughts
-  pandoc --standalone $STAGING_DIR/lbc1689-toc.md --to html --output $HTML_DIR/lbc1689-toc
-  pandoc --standalone $STAGING_DIR/lbc1689-introduction.md --to html --output $HTML_DIR/lbc1689-introduction
+  pandoc --standalone $STAGING_DIR/aab-TitlePage.md --to html --output $HTML_DIR/aab-TitlePage --css $STYLES_DIR/styles.css
+  pandoc --standalone $STAGING_DIR/aab-OpeningThoughts.md --to html --output $HTML_DIR/aab-OpeningThoughts --css $STYLES_DIR/styles.css
+  pandoc --standalone $STAGING_DIR/lbc1689-toc.md --to html --output $HTML_DIR/lbc1689-toc --css $STYLES_DIR/styles.css
+  pandoc --standalone $STAGING_DIR/lbc1689-introduction.md --to html --output $HTML_DIR/lbc1689-introduction --css $STYLES_DIR/styles.css
   # Concatenate all chapter files
   index=0
   for chapter in "$STAGING_DIR"/lbc1689-ch*.md; do
       index=$((index + 1))
       padded_index=$(printf "%02d" $index)
-      pandoc --standalone "${chapter}" --to html --output "$HTML_DIR/lbc1689-ch$padded_index"
+      pandoc --standalone "${chapter}" --to html --output "$HTML_DIR/lbc1689-ch$padded_index" --css $STYLES_DIR/styles.css
   done
-  pandoc --standalone $STAGING_DIR/lbc1689-signatories.md --to html --output $HTML_DIR/lbc1689-signatories
-  pandoc --standalone $STAGING_DIR/aab-Addendum.md --to html --output $HTML_DIR/aab-Addendum
+  pandoc --standalone $STAGING_DIR/lbc1689-signatories.md --to html --output $HTML_DIR/lbc1689-signatories --css $STYLES_DIR/styles.css
+  pandoc --standalone $STAGING_DIR/aab-Addendum.md --to html --output $HTML_DIR/aab-Addendum --css $STYLES_DIR/styles.css
 }
 
 prepareMarkdownForPdf() {
@@ -61,7 +62,7 @@ prepareMarkdownForPdf() {
 
 buildPdf() {
   prepareMarkdownForPdf
-  pandoc $STAGING_DIR/aab-TitlePage.md $STAGING_DIR/aab-OpeningThoughts.md $STAGING_DIR/lbc1689-toc.md $STAGING_DIR/lbc1689-introduction.md $(ls $STAGING_DIR/lbc1689-ch*.md) $STAGING_DIR/lbc1689-signatories.md $STAGING_DIR/aab-Addendum.md --output $GENERATED_PDF
+  pandoc $STAGING_DIR/aab-TitlePage.md $STAGING_DIR/lbc1689-toc.md $STAGING_DIR/aab-OpeningThoughts.md $STAGING_DIR/lbc1689-introduction.md $(ls $STAGING_DIR/lbc1689-ch*.md) $STAGING_DIR/lbc1689-signatories.md $STAGING_DIR/aab-Addendum.md --output $GENERATED_PDF --css $STYLES_DIR/styles.css
 }
 
 init
